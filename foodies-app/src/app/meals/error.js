f10ff491bd4befd326929bbb5f73084aa24c0823
varunpats <varunpatil2502@gmail.com>
2024-01-22 00:51:13 +0530

    Did error handling

---
+++ foodies-app/src/app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error() {
+    return(
+        <main className="error">
+            <h1>An error occured</h1>
+            <p>Unable to fetch meals data. Try again later.</p>
+        </main>
+    )
+}
