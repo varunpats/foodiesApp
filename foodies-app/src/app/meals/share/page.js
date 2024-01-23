f704cd2f670211b7f42b53e2baff917efdf3e84c
varunpats <varunpatil2502@gmail.com>
2024-01-23 15:26:59 +0530

    Code refactoring

--- foodies-app/src/app/meals/share/page.js
+++ foodies-app/src/app/meals/share/page.js
@@ -1,19 +1,8 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '../../../../lib/action';
 
 export default function ShareMealPage() {
-    async function shareMeal(formData) {
-        'use server'
-
-        const meal = {
-            title: formData.get('title'),
-            summary: formData.get('summary'),
-            instructions: formData.get('instructions'),
-            image: formData.get('image'),
-            creator: formData.get('name'),
-            creator_email: formData.get('email'),
-        }
-    }
     return (
         <>
             <header className={classes.header}>
