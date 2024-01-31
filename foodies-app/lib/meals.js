ff13d245b6891aa06524525dd2d911f03f5163e2
varunpats <varunpatil2502@gmail.com>
2024-01-31 11:47:14 +0530

    Adding data to DB

--- foodies-app/lib/meals.js
+++ foodies-app/lib/meals.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import sql from 'better-sqlite3';
 import slugify from 'slugify';
 import xss from 'xss';
@@ -13,10 +14,31 @@ export function getMeal(slug) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
 }
 
-export function saveMeal(meal) {
+export async function saveMeal(meal) {
     meal.slug = slugify(meal.title, { lower: true })
-    meal.instructions = xss(instructions)
+    meal.instructions = xss(meal.instructions)
 
-    const extension = meal.image.split('.').pop();
-    const filename = `${meal.slug}.${extension}`
+    const extension = meal.image?.name.split('.').pop();
+    const filename = `${meal.slug}.${extension}`;
+
+    const stream = fs.createWriteStream(`public/images/${filename}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error('Saving image failed!')
+        }
+    });
+
+    meal.image = `/images/${filename}`
+
+    db.prepare(`INSERT INTO meals (title, summary, instructions, creator, creator_email) VALUES (
+        @title,
+        @summary,
+        @instructions,
+        @creator,
+        @creator_email,
+        @image,
+        @slug
+        )`)
 }
